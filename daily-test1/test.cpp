51ff0b17b2fe85eef245130a013a450639a28ad1
wlkkt <yangyf0618@qq.com>
2024-04-19 23:51:20 +0800

    更新

--- daily-test1/test.cpp
+++ daily-test1/test.cpp
@@ -26,34 +26,198 @@
 //private:
 //	int _b;
 //};
-///////////////////////////////////////////多态：买票
+///////////////////////////////////////////多态：析构函数重写
+
+////普通场景:是否是虚函数都无所谓
 //#include <iostream>
 //using namespace std;
+//class A {};
+//class B : public A {};
 //
-//class Person
+//class Person {
+//public:
+//	virtual A* BuyTicket() 
+//	{
+//		cout << "买票-全价" << endl;
+//		return nullptr;
+//	}
+//
+//	 ~Person() 
+//	{ 
+//		cout << "~Person()" << endl; 
+//	}
+//};
+//
+//class Student : public Person 
 //{
 //public:
-//	virtual void BuyTicket() { cout << "Person->买票->全价" << endl; }
+//	virtual B* BuyTicket() 
+//	{
+//		cout << "买票-半价" << endl;
+//		return nullptr;
+//	}
+//
+//	~Student()
+//	{ 
+//		cout << "~Student()" << endl;
+//	}
+//};
+//
+//int main()
+//{
+//	Person p;	
+//	Student s;
+//	return 0;
+//}
+
+
+////特殊场景：初阶
+//#include <iostream>
+//using namespace std;
+//class A {};
+//class B : public A {};
+//
+//class Person {
+//public:
+//	virtual A* BuyTicket()
+//	{
+//		cout << "买票-全价" << endl;
+//		return nullptr;
+//	}
+//
+//	~Person()
+//	{
+//		cout << "~Person()" << endl;
+//	}
 //};
 //
 //class Student : public Person
 //{
 //public:
-//	virtual void BuyTicket() { cout << "Student->买票->半价" << endl; }
-//};
+//	virtual B* BuyTicket()
+//	{
+//		cout << "买票-半价" << endl;
+//		return nullptr;
+//	}
 //
-////void Func(Person* p)
-//void Func(Person& p)
-//{
-//	p.BuyTicket();
-//}
+//	~Student()
+//	{
+//		cout << "~Student()" << endl;
+//	}
+//};
 //
 //int main()
 //{
-//	Person ps;
-//	Student stu;
+//	//基类类型的指针，既可以指向基类对象也可以指向派生类对象，对派生类而言是因为有切片切割的概念存在
+//	Person* p1 = new Person;
+//	Person* p2 = new Student;
 //
-//	Func(ps);
-//	Func(stu);
+//	delete p1;
+//	delete p2;
+//
 //	return 0;
 //}
+
+
+//特殊场景：延申1
+//#include <iostream>
+//using namespace std;
+//class A {};
+//class B : public A {};
+//
+//class Person {
+//public:
+//	virtual A* BuyTicket()
+//	{
+//		cout << "买票-全价" << endl;
+//		return nullptr;
+//	}
+//
+//	virtual ~Person()
+//	{
+//		cout << "~Person()" << endl;
+//	}
+//};
+//
+//class Student : public Person
+//{
+//public:
+//	virtual B* BuyTicket()
+//	{
+//		cout << "买票-半价" << endl;
+//		return nullptr;
+//	}
+//
+//	virtual ~Student() //派生类的成员指针因为派生类的析构函数未被调用，该指针指向的空间不会被释放，内存泄漏
+//	{
+//		delete[] _ptr;
+//		cout << "~Student()" << endl;
+//	}
+//private:
+//	int* _ptr = new int[10];
+//};
+//
+//int main()
+//{
+//	//基类类型的指针，既可以指向基类对象也可以指向派生类对象，对派生类而言是因为有切片切割的概念存在
+//	Person* p1 = new Person;
+//	Person* p2 = new Student;
+//
+//	delete p1;
+//	delete p2;
+//
+//	return 0;
+//}
+//
+
+
+//派生类的虚函数可以不加virtual
+#include <iostream>
+using namespace std;
+class A {};
+class B : public A {};
+
+class Person {
+public:
+	virtual A* BuyTicket()
+	{
+		cout << "买票-全价" << endl;
+		return nullptr;
+	}
+
+	virtual ~Person()
+	{
+		cout << "~Person()" << endl;
+	}
+};
+
+class Student : public Person
+{
+public:
+	virtual B* BuyTicket()
+	{
+		cout << "买票-半价" << endl;
+		return nullptr;
+	}
+
+	 ~Student() //派生类的成员指针因为派生类的析构函数未被调用，该指针指向的空间不会被释放，内存泄漏
+	{
+		delete[] _ptr;
+		cout << "~Student()" << endl;
+	}
+private:
+	int* _ptr = new int[10];
+};
+
+int main()
+{
+	//基类类型的指针，既可以指向基类对象也可以指向派生类对象，对派生类而言是因为有切片切割的概念存在
+	Person* p1 = new Person;
+	Person* p2 = new Student;
+
+	delete p1;
+	delete p2;
+
+	return 0;
+}
+
